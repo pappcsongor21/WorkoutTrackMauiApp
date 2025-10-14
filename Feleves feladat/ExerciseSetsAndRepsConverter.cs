18d263abe31a8dd2dc023d9afdee987a697f282f
Csongor Papp <papp.csongor221@gmail.com>
2025-10-14 21:14:32 +0200

    WorkoutPage got a minimal working look

---
+++ Feleves feladat/ExerciseSetsAndRepsConverter.cs
@@ -0,0 +1,27 @@
+﻿using Feleves_feladat.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feleves_feladat
+{
+    public class ExerciseSetsAndRepsConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if(value is Exercise)
+            {
+                return $"{((Exercise)value).TargetSets} X {((Exercise)value).TargetReps}";
+            }
+            return string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
