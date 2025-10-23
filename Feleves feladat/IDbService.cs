aa947b6b3cf999dc06a583c7c5431dac5ed07d43
Csongor Papp <papp.csongor221@gmail.com>
2025-10-23 19:40:56 +0200

    crud methods in DbService and IDbService

--- Feleves feladat/IDbService.cs
+++ Feleves feladat/IDbService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Feleves_feladat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Feleves_feladat
 {
     public interface IDbService
     {
+        Task<List<Workout>> GetWorkoutsAsync();
+        Task<int> CreateWorkoutAsync(Workout workout);
+        Task<int> UpdateWorkoutAsync(Workout workout);
+        Task<int> DeleteWorkoutAsync(Workout workout);
+
+        Task<List<Exercise>> GetExercisesAsync();
+        Task<List<Exercise>> GetExercisesByWorkoutIdAsync(int workoutId);
+        Task<int> CreateExerciseAsync(Exercise exercise);
+        Task<int> UpdateExerciseAsync(Exercise exercise);
+        Task<int> DeleteExerciseAsync(Exercise exercise); 
+
+        Task<List<PerformedSet>> GetPerformedSetsAsync();
+        Task<List<PerformedSet>> GetPerformedSetsByExerciseIdAsync(int exerciseId);
+        Task<int> CreatePerformedSetAsync(PerformedSet performedSet);
+        Task<int> UpdatePerformedSetAsync(PerformedSet performedSet);
+        Task<int> DeletePerformedSetAsync(PerformedSet performedSet);
+
     }
 }
--- Feleves feladat/Services/DbService.cs
+++ Feleves feladat/Services/DbService.cs
@@ -21,8 +21,70 @@ namespace Feleves_feladat.Services
             db.CreateTableAsync<PerformedSet>().Wait();
             db.CreateTableAsync<ExerciseTemplate>().Wait();
 
-            App.Current.MainPage.DisplayAlert("DB Path", dbPath, "OK");
             Debug.WriteLine($" SQLite database path: {dbPath}");
         }
+
+        #region:WorkoutCRUD
+        public Task<List<Workout>> GetWorkoutsAsync()
+        {
+            return db.Table<Workout>().ToListAsync();
+        }
+        public Task<int> CreateWorkoutAsync(Workout workout)
+        {
+            return db.InsertAsync(workout);
+        }
+        public Task<int> UpdateWorkoutAsync(Workout workout)
+        {
+            return db.UpdateAsync(workout);
+        }
+        public Task<int> DeleteWorkoutAsync(Workout workout)
+        {
+            return db.DeleteAsync(workout);
+        }
+        #endregion
+        #region:ExerciseCRUD
+        public Task<List<Exercise>> GetExercisesAsync()
+        {
+            return db.Table<Exercise>().ToListAsync();
+        }
+        public Task<List<Exercise>> GetExercisesByWorkoutIdAsync(int workoutId)
+        {
+            return db.Table<Exercise>().Where(e => e.WorkoutId == workoutId).ToListAsync();
+        }
+        public Task<int> CreateExerciseAsync(Exercise exercise)
+        {
+            return db.InsertAsync(exercise);
+        }
+        public Task<int> UpdateExerciseAsync(Exercise exercise)
+        {
+            return db.UpdateAsync(exercise);
+        }
+        public Task<int> DeleteExerciseAsync(Exercise exercise)
+        {
+            return db.DeleteAsync(exercise);
+        }
+        #endregion
+        #region:PerformedSetCRUD
+        public Task<List<PerformedSet>> GetPerformedSetsAsync()
+        {
+            return db.Table<PerformedSet>().ToListAsync();
+        }
+        public Task<List<PerformedSet>> GetPerformedSetsByExerciseIdAsync(int exerciseId)
+        {
+            return db.Table<PerformedSet>().Where(e => e.ExerciseId == exerciseId).ToListAsync();
+        }
+        public Task<int> CreatePerformedSetAsync(PerformedSet performedSet)
+        {
+            return db.InsertAsync(performedSet);
+        }
+        public Task<int> UpdatePerformedSetAsync(PerformedSet performedSet)
+        {
+            return db.UpdateAsync(performedSet);
+        }
+        public Task<int> DeletePerformedSetAsync(PerformedSet performedSet)
+        {
+            return db.DeleteAsync(performedSet);
+        }
+        #endregion
     }
 }
